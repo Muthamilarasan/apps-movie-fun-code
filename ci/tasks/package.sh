1a2e7257f45989983acd9bcd30a884c6d216807f
muthamil <muthamil@vnameits-MacBook-Pro.local>
2017-11-03 15:50:54 +0530

    changed

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -14,12 +14,6 @@ pushd movie_fun_source
    mvn clean package -DskipTests
 popd
 
-jar_count=`find movie_fun_source/target -type f -name *.jar | wc -l`
-
-if [ $jar_count -gt 1 ]; then
-  echo "More than one jar found, don't know which one to deploy. Exiting"
-  exit 1
-fi
 
 find movie_fun_source/target -type f -name *.jar -exec cp "{}" package-output/movie_fun.jar \;
 
